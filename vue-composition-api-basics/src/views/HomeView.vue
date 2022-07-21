d4050f8897cf69325789ff735c494602d3873709
FangtaiDong10 <fangtaidong10@gmail.com>
2022-07-21 11:50:32 +1000

    computed and watch practise in Vue3

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -1,73 +1,60 @@
 <template>
   <div class="home">
+    <h2>{{ appTitle }}</h2>
 
-    <h3>{{counterTitle}}</h3>
+    <h3>{{ counterData.title }}:</h3>
 
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
+    </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-const counter = ref(0), counterTitle = ref("My Counter");
+import { ref, reactive, computed, watch } from "vue";
 
-const increaseCounter = () => {
-  counter.value++;
+const appTitle = "My Amazing Counte App OK";
+
+const counterData = reactive({
+  count: 0,
+  title: "My Counter",
+});
+
+const count = ref();
+
+watch(() => counterData.count, (newCount, oldCount) => {
+  // console.log('newCount', newCount);
+  if (newCount === 20) {
+    alert('Way to go! You made it to 20!!')
+  }
+});
+
+// computed property
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 === 0) return "even";
+  return "odd";
+});
+
+const increaseCounter = (amount, e) => {
+  console.log(e);
+
+  counterData.count += amount;
 };
-const decreaseCounter = () => {
-  counter.value--;
-};
-</script>
-
-<!--
-<script>
-import { ref } from "vue";
-
-export default {
-  setup() {
-    const counter = ref(0);
-
-    const increaseCounter = () => {
-      counter.value++;
-    };
-    const decreaseCounter = () => {
-      counter.value--;
-    };
-
-    return {
-      counter,
-      increaseCounter,
-      decreaseCounter
-
-    };
-  },
+const decreaseCounter = (amount) => {
+  counterData.count -= amount;
 };
 </script>
--->
-
-<!-- option API
-<script>
-export default {
-  data() {
-    return {
-      counter: 0,
-    };
-  },
-  methods: {
-    increaseCounter() {
-      this.counter++;
-    },
-    decreaseCounter() {
-      this.counter--;
-    },
-  },
-};
-</script>
--->
 
 <style>
 .home {
@@ -79,5 +66,9 @@ export default {
 .counter {
   font-size: 40px;
   margin: 10px;
+}
+
+.edit {
+  margin: 60px;
 }
 </style>
