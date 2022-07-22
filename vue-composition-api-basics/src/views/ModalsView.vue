8042ac748f164d7abfb25c9d71529fb40748dd4b
FangtaiDong10 <fangtaidong10@gmail.com>
2022-07-22 21:11:33 +1000

    teleport element and CSS initial keyword

--- vue-composition-api-basics/src/views/ModalsView.vue
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -2,18 +2,20 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <div
-      v-if="showModal"
-      class="modal">
-      <h1>This is a modal</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab asperiores,
-        sapiente ut harum adipisci corrupti cupiditate distinctio impedit a sint
-        placeat minima accusamus maiores eum similique voluptate illum saepe
-        odio.
-      </p>
-      <button @click="showModal = false">Hide modal</button>
-    </div>
+
+    <teleport to=".modals-container">
+      <div v-if="showModal" class="modal">
+        <h1>This is a modal</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+          asperiores, sapiente ut harum adipisci corrupti cupiditate distinctio
+          impedit a sint placeat minima accusamus maiores eum similique
+          voluptate illum saepe odio.
+        </p>
+        <button @click="showModal = false">Hide modal</button>
+      </div>
+    </teleport>
+
   </div>
 </template>
 
@@ -21,19 +23,23 @@
 /**
  * imports
  */
-  import { ref } from 'vue';
+import { ref } from "vue";
 
 /**
  * modals
  */
-const showModal = ref(false)
-
-
+const showModal = ref(false);
 </script>
 
 <style>
-.modal{
+.modal {
   background: darkgreen;
   padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 }
 </style>
