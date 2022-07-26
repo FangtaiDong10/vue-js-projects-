06ba16bd1f9bc2ad3a53735543398e90eb7d0b5b
FangtaiDong10 <fangtaidong10@gmail.com>
2022-07-26 17:37:16 +1000

    Note component and propsDefine()

--- vue-composition-api-basics/src/views/ViewNotes.vue
+++ vue-composition-api-basics/src/views/ViewNotes.vue
@@ -23,24 +23,24 @@
           </button>
         </div>
       </div>
+
+
+      <Note
+        v-for="note in notes"
+        :key="note.id" 
+        :note="note"
+        />
+
     </div>
 
-    <div v-for="note in notes" :key="note.id" class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-          {{ note.content }}
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import Note from "@/components/Notes/Note.vue"
+
 
 /**
  * notes
