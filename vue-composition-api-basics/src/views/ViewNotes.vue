15493c38ea95e6c43e1125cc206b16c8b40394fc
FangtaiDong10 <fangtaidong10@gmail.com>
2022-07-26 17:17:00 +1000

    add note card after input

--- vue-composition-api-basics/src/views/ViewNotes.vue
+++ vue-composition-api-basics/src/views/ViewNotes.vue
@@ -3,26 +3,32 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
             Add New Note
           </button>
         </div>
       </div>
     </div>
 
-    <div
-      v-for="note in notes"
-      :key="note.id"
-      class="card mb-4">
+    <div v-for="note in notes" :key="note.id" class="card mb-4">
       <div class="card-content">
         <div class="content">
-          {{note.content}}
+          {{ note.content }}
         </div>
       </div>
       <footer class="card-footer">
@@ -39,14 +45,35 @@ import { ref } from "vue";
 /**
  * notes
  */
+
+const newNote = ref("");
+
+const newNoteRef = ref(null);
+
 const notes = ref([
   {
-    id:'id1',
-    content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores quasi tempora nisi aut veritatis, magnam sit? Rem eos modi sapiente reprehenderit sint cupiditate dolorum laborum maiores corrupti, dolore, commodi unde!'
+    id: "id1",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores quasi tempora nisi aut veritatis, magnam sit? Rem eos modi sapiente reprehenderit sint cupiditate dolorum laborum maiores corrupti, dolore, commodi unde!",
   },
   {
-    id:'id2',
-    content: 'This is a shorter note!'
-  }
+    id: "id2",
+    content: "This is a shorter note!",
+  },
 ]);
+
+const addNote = () => {
+  let currentDate = new Date().getTime(),
+    id = currentDate.toString();
+
+  let note = {
+    id,
+    content: newNote.value,
+  };
+
+  // push what input into notes list
+  notes.value.unshift(note);
+  newNote.value = "";
+  newNoteRef.value.focus();
+};
 </script>
