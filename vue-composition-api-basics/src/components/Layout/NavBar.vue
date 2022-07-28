aa5c8f1dcbc928e1f0760f12edaabeeb7e2c14a5
FangtaiDong10 <fangtaidong10@gmail.com>
2022-07-28 19:44:14 +1000

    stat table complete

--- vue-composition-api-basics/src/components/Layout/NavBar.vue
+++ vue-composition-api-basics/src/components/Layout/NavBar.vue
@@ -36,36 +36,23 @@
         :class="{ 'is-active': showMobileNav }"
       >
         <div class="navbar-start">
-          <RouterLink to="/" class="navbar-item" active-class="is-active">
+          <RouterLink
+            @click="showMobileNav = false"
+            to="/"
+            class="navbar-item"
+            active-class="is-active">
             Notes
           </RouterLink>
 
-          <RouterLink to="/stats" class="navbar-item" active-class="is-active">
-            Documentation
+          <RouterLink
+            @click="showMobileNav = false"
+            to="/stats"
+            class="navbar-item"
+            active-class="is-active">
+            Stats
           </RouterLink>
 
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link"> More </a>
-
-            <div class="navbar-dropdown">
-              <a class="navbar-item"> About </a>
-              <a class="navbar-item"> Jobs </a>
-              <a class="navbar-item"> Contact </a>
-              <hr class="navbar-divider" />
-              <a class="navbar-item"> Report an issue </a>
-            </div>
-          </div>
-        </div>
-
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <a class="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a class="button is-light"> Log in </a>
-            </div>
-          </div>
+          
         </div>
       </div>
     </div>
@@ -79,6 +66,7 @@ import { ref } from "vue";
  * mobile nav
  */
 const showMobileNav = ref(false);
+
 </script>
 
 <style>
--- vue-composition-api-basics/src/stores/storeNotes.js
+++ vue-composition-api-basics/src/stores/storeNotes.js
@@ -49,5 +49,15 @@ export const useStoreNotes = defineStore("storeNotes", {
         })[0].content;
       };
     },
+    totalNotesCount: (state) => {
+      return state.notes.length;
+    },
+    totalCharactersCount: (state) => {
+      let count = 0;
+      state.notes.forEach((note) => {
+        count += note.content.length;
+      });
+      return count;
+    },
   },
 });
